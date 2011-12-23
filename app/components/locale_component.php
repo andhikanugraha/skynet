0be80e73d4c5b7bfcc32216f676f61205d0f8dee
Andhika Nugraha <andhika.nugraha@gmail.com>
2011-12-23 12:49:57 +0700

    Updated LocaleComponent
    Added localization for FormDisplay.

--- app/components/locale_component.php
+++ app/components/locale_component.php
@@ -1,32 +1,87 @@
 <?php
 
+/**
+ * LocaleComponent
+ *
+ * Application localization
+ *
+ * @author Andhika Nugraha <andhika.nugraha@gmail.com>
+ * @package locale
+ */
 class LocaleComponent extends HeliumComponent {
+	$translations = array(
+		'Monday' => 'Senin',
+		'Tuesday' => 'Selasa',
+		'Wednesday' => 'Rabu',
+		'Thursday' => 'Kamis',
+		'Friday' => 'Jumat',
+		'Saturday' => 'Sabtu',
+		'Sunday' => 'Minggu',
+		'January' => 'Januari',
+		'February' => 'Februari',
+		'March' => 'Maret',
+		'May' => 'Mei',
+		'June' => 'Juni',
+		'July' => 'Juli',
+		'August' => 'Agustus',
+		'October' => 'Oktober',
+		'December' => 'Desember',
+		'(Day)' => '(Tanggal)',
+		'(Month)' => '(Bulan)',
+		'(Year)' => '(Tahun)',
+		'Town' => 'Kota',
+		'State/Province' => 'Provinsi'
+		'Postcode' => 'Kode Pos',
+		'Mobile Phone' => 'HP',
+		'Home Phone' => 'Telepon',
+		'Fax' => 'Faks'
+	);
 	
 	public function init() {
-		// do two things:
-		// 1. add a locale for translations
-		// 2. set a local timezone
+		$translations = $this->translations;
 
-		$translations = array(
-			'Monday' => 'Senin',
-			'Tuesday' => 'Selasa',
-			'Wednesday' => 'Rabu',
-			'Thursday' => 'Kamis',
-			'Friday' => 'Jumat',
-			'Saturday' => 'Sabtu',
-			'Sunday' => 'Minggu',
-			'January' => 'Januari',
-			'February' => 'Februari',
-			'March' => 'Maret',
-			'May' => 'Mei',
-			'June' => 'Juni',
-			'July' => 'Juli',
-			'August' => 'Agustus',
-			'October' => 'Oktober',
-			'December' => 'Desember'
-		);
 		HeliumDateTime::add_locale('id', $translations);
 		HeliumDateTime::set_default_timezone(Helium::conf('site_timezone'));
+
+		foreach ($translations as $k => $v) {
+			FormDisplay::$strings[$k] = $v;
+		}
+		
+		FormDisplay::$provinces = array(
+			'Nangroe Aceh Darussalam',
+			'Sumatera Utara',
+			'Sumatera Barat',
+			'Riau',
+			'Jambi',
+			'Sumatera Selatan',
+			'Bengkulu',
+			'Lampung',
+			'Kepulauan Bangka Belitung',
+			'Kepulauan Riau',
+			'DKI Jakarta',
+			'Jawa Barat',
+			'Jawa Tengah',
+			'DI Yogyakarta',
+			'Jawa Timur',
+			'Banten',
+			'Bali',
+			'Nusa Tenggara Barat',
+			'Nusa Tenggara Timur',
+			'Kalimantan Barat',
+			'Kalimantan Tengah',
+			'Kalimantan Selatan',
+			'Kalimantan Timur',
+			'Sulawesi Utara',
+			'Sulawesi Tengah',
+			'Sulawesi Selatan',
+			'Sulawesi Tenggara',
+			'Gorontalo',
+			'Sulawesi Barat',
+			'Maluku',
+			'Maluku Utara',
+			'Papua Barat',
+			'Papua'
+		);
 	}
 	
 }
