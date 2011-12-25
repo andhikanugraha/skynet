4380ec09944f2bd8ad1a54dcb613e86cb35a9fb8
Andhika Nugraha <andhika.nugraha@gmail.com>
2011-12-25 07:30:59 +0700

    Modified HomeController
    Added installer script for all chapters.

--- app/controllers/home_controller.php
+++ app/controllers/home_controller.php
@@ -18,6 +18,61 @@ class HomeController extends GatotkacaController {
 			$user->role = 5;
 			$user->chapter_id = $chapter->id;
 			$user->save();
+
+			$chapters = array (
+				'DPS' => 'Bali',
+				'BPP' => 'Balikpapan',
+				'BNA' => 'Banda Aceh',
+				'BDG' => 'Bandung',
+				'BMS' => 'Banjarmasin',
+				'BGR' => 'Bogor',
+				'JKT' => 'Jakarta',
+				'KRW' => 'Karawang',
+				'MKS' => 'Makassar',
+				'MLG' => 'Malang',
+				'MDN' => 'Medan',
+				'PDG' => 'Padang',
+				'PLM' => 'Palembang',
+				'SMD' => 'Samarinda',
+				'SMG' => 'Semarang',
+				'SUB' => 'Surabaya',
+				'JOG' => 'Yogyakarta',
+			);
+			
+			$timezones = array (
+				'DPS' => 'Asia/Ujung_Pandang',
+				'BPP' => 'Asia/Ujung_Pandang',
+				'BNA' => 'Asia/Jakarta',
+				'BDG' => 'Asia/Jakarta',
+				'BMS' => 'Asia/Ujung_Pandang',
+				'BGR' => 'Asia/Jakarta',
+				'JKT' => 'Asia/Jakarta',
+				'KRW' => 'Asia/Jakarta',
+				'MKS' => 'Asia/Ujung_Pandang',
+				'MLG' => 'Asia/Jakarta',
+				'MDN' => 'Asia/Jakarta',
+				'PDG' => 'Asia/Jakarta',
+				'PLM' => 'Asia/Jakarta',
+				'SMD' => 'Asia/Ujung_Pandang',
+				'SMG' => 'Asia/Jakarta',
+				'SUB' => 'Asia/Jakarta',
+				'JOG' => 'Asia/Jakarta',
+			);
+			
+			foreach ($chapters as $code => $name) {
+				$chapter = new Chapter;
+				$chapter->chapter_code = $code;
+				$chapter->chapter_name = $name;
+				$chapter->chapter_timezone = $timezones[$code];
+				$chapter->save();
+				
+				$user = new User;
+				$user->username = 'chapter_' . strtolower(str_replace(' ', '_', $name));
+				$user->set_password('antarbudaya');
+				$user->role = 4;
+				$user->chapter_id = $chapter->id;
+				$user->save();
+			}
 		}
 	}
 
