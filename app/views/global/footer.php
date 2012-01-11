614c7f660f6ee8ae2009a27246b5e0a76daf7f81
Andhika Nugraha <andhika.nugraha@gmail.com>
2012-01-11 10:52:55 +0700

    Added AFS Partner footer

--- app/views/global/footer.php
+++ app/views/global/footer.php
@@ -1,6 +1,7 @@
 		</div>
 		<footer class="global-footer">
 			<!-- Untuk bantuan, baca <strong><a href="<?php L(array('controller' => 'applicant', 'action' => 'guide')); ?>">panduan</a></strong>, mention/DM <strong>@afsbandung</strong> atau e-mail <strong>seleksi@binabudbdg.org</strong>. -->
+			<img src="<?php L('/assets/images/AFS_Partner.png') ?>" alt="A Partner of AFS Intercultural Programs">
 		</footer>
 	</body>
 
