af5502f8d40dab287a945357948f2f1d80c17a7e
Andhika Nugraha <andhika.nugraha@gmail.com>
2012-01-15 15:02:24 +0700

    Removed AFS logo from footer

--- app/views/global/footer.php
+++ app/views/global/footer.php
@@ -1,7 +1,7 @@
 		</div>
 		<footer class="global-footer">
 			<!-- Untuk bantuan, baca <strong><a href="<?php L(array('controller' => 'applicant', 'action' => 'guide')); ?>">panduan</a></strong>, mention/DM <strong>@afsbandung</strong> atau e-mail <strong>seleksi@binabudbdg.org</strong>. -->
-			<address class="afs-partner"><img src="<?php L('/assets/images/AFS_Partner.png') ?>" alt="A Partner of AFS Intercultural Programs"></address>
+			<!-- <address class="afs-partner"><img src="<?php L('/assets/images/AFS_Partner.png') ?>" alt="A Partner of AFS Intercultural Programs"></address> -->
 		</footer>
 	</body>
 
